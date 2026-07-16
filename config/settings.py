44559e42cea420c82900077cbfd24feab7e1ab87
Jaswanth-12 <90079855+Jaswanth-12@users.noreply.github.com>
2026-07-16 21:22:34 +0530

    Add files via upload

---
+++ config/settings.py
@@ -0,0 +1,14 @@
+# Purpose: Store all configuration settings in one place (e.g., database credentials, file paths).
+
+# Define Configuration (settings.py)
+DB_CONFIG = {
+    "host": "",  
+    "port": 5324 ,         
+    "database": "",  
+    "user": "",      
+    "password": ""   
+}
+
+
+INPUT_FILE = "data/input/extra_data.parquet"
+OUTPUT_FILE = "data/output/transformed_data.csv"
